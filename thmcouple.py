c6cbed499f1bc728e4c013f3259e05da47b08bcd
PeterFeng1997 <yudong.feng@mail.utoronto.ca>
2017-05-24 23:40:22 -0400

    Update thmcouple.py
    Rewrote parts of some functions to avoid using global variables

--- thmcouple.py
+++ thmcouple.py
@@ -2,7 +2,7 @@
 
     ************************************************************************
     *   FILE NAME:      thmcouple.py
-    *   AUTHOR:         Dylan Vogel
+    *   AUTHORS:         Dylan Vogel, Peter Feng
     *   PURPOSE:        This file contains functions related to thermocouple
     *                   measurement via the MAX31855.
     *
@@ -25,6 +25,19 @@ global tc_1, tc_2
 
 READBYTE = [0x00, 0x00]
 
+def setup1():
+   tc_1 = spidev.SpiDev()
+   tc_1.open(0,0)
+   tc_1.max_speed_hz = 5000000
+   return tc_1
+
+def setup2():
+   tc_2 = spidev.SpiDev()
+   tc_2.open(0,1)
+   tc_2.max_speed_hz = 5000000
+   return tc_2
+
+'''
 def setup():
     global tc_1, tc_2
 
@@ -34,14 +47,37 @@ def setup():
     tc_2.open(0,1)
     tc_1.max_speed_hz = 5000000    # See MAX31855 datasheet
     tc_2.max_speed_hz = 5000000;
+'''
 
-
-def read(thmcouple):
+'''def read(thmcouple):
     # Assume the input is either 1 or 2, and default to 1 if another value is entered.
     if (thmcouple == 2):
         rec = tc_2.xfer2(READBYTE)
     else:
         rec = tc_1.xfer2(READBYTE)
+
+    # Check the fault bit of the returned message
+    if rec[1] & 0x01:
+        fault = 1
+        #print "A fault was encountered by the MAX31855"
+    else:
+        fault = 0
+
+    # Convert the 16-bit returned number to the 14-bit temp value.
+    data = (rec[0] << 6) | (rec[1] >> 2)
+
+    # Check for negatives
+    if data >> 13:
+        temp = float((int(data) / 4.0) - 2048)
+    else:
+        temp = float(data / 4.0)
+
+    return temp'''
+
+def read(thmcouple):
+    # Assume the input is either 1 or 2, and default to 1 if another value is entered.
+         
+    rec = thmcouple.xfer2(READBYTE)
 
     # Check the fault bit of the returned message
     if rec[1] & 0x01:
