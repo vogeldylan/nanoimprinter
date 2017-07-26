e2a5b3b303f15a04d2a63cfb35512e21b72d9282
vogeldylan <vogel.sohei@gmail.com>
2017-07-26 15:41:54 +0800

    Added RTD interfacing functions, temporarily kept in thmcouple.py

--- main.py
+++ main.py
@@ -44,15 +44,16 @@ import dataLog as log
 import thmcouple as thm
 import heater
 import PID
-
+import controller
 ################################################################################
 ''' EDIT THESE; THESE ARE THE PID COEFFICIENTS '''
 
+'''
 
-def pid_setup_center(work_temp):
+def pid_setup_center(work_temp, P, I, D): #change
 
     # Written as (Kp, Ki, Kd)
-    pid_center = PID.PID(4, 0, 2)
+    pid_center = PID.PID(P, I, D)
 
     # Windup to prevent integral term from going too high/low.
     pid_center.setWindup(1)
@@ -63,190 +64,39 @@ def pid_setup_center(work_temp):
 
     return pid_center
 
-def pid_setup_edge(work_temp):
+def pid_setup_edge(work_temp, P, I, D): #change
 
-    pid_edge = PID.PID(12, 0, 2)
+    pid_edge = PID.PID(P, I, D)
 
     pid_edge.setWindup(1)
     pid_edge.setSampleTime(0.5)
     pid_edge.SetPoint = work_temp
 
     return pid_edge
-
+'''
 
 if __name__ == "__main__":
 
     ################################################################################
     ''' EDIT THESE SETUP VARIABLES '''
 
-    work_temp = input('Enter a working temperature between 0-200 C: ')
-    #heat_time = input('Enter bonding time in seconds 0-3600s: ')
+    #change
+    #work_temp = input('Enter a working temperature between 0-200 C: ')
 
-    # The frequency at which main.py will write to the console and log file.
-    data_log_freq = 1
+    app = controller.QtWidgets.QApplication(sys.argv)
+    window = controller.MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
-    # The PWM duty used for the initial heating. Generally around 50% for the
-    # center and 100% for the edge seems to work. Should be adjusted based on
-    # changes to the thermal volume based on empirical results.
-    pwm_center = 40
-    pwm_edge = 100
+    work_temp = window.temp
+    P_center = window.P_center
+    I_center = window.I_center
+    D_center = window.D_center
 
-    # Used to suppress Kp as it approaches the setpoint.
-    limited_kp =  1 #heater.calc_kp(work_temp)
-
-    limited_kd = -0.2
-
-    # Time to wait after initial heating for temp to settle
-    wait_time = 30
-
-    ################################################################################
-    ''' DON'T EDIT THESE UNLESS YOU KNOW WHAT YOU'RE DOING '''
-
-    log.setup("PID_cartridge_test")
-
-    thm1 = thm.setup1()
-    thm2 = thm.setup2()
-
-    pwm_1 = heater.setup1()
-    pwm_2 = heater.setup2()
-
-    pid_edge = pid_setup_edge(work_temp)
-    pid_center = pid_setup_center(work_temp)
-
-    pid_edge_val = pid_edge.getPID()
-    pid_center_val = pid_center.getPID()
-
-    # These variables will have current temp values written to them.
-    t_center = thm.read(thm1)
-    t_edge = thm.read(thm2)
-
-    # These variables store the temperature averaged over the last two values.
-    t_center_avg = t_center
-    t_edge_avg = t_edge
-
-    # Variables used for timekeeping and data plotting.
-    start_t = time.time()
-    curr_t = time.time()
-
-    times = []
-    cent_temps = []
-    edge_temps = []
-
-
-    print ("Setup completed, initial heating  ... ")
-
-    try:
-        # Function stored in heater.py. Algorithm based on empirical results.
-        heat_time = heater.initial_heating_time(t_center, t_edge, work_temp, thm1, thm2)
-        heater.change_duty(pwm_center, pwm_edge, pwm_1, pwm_2)
-        # This is the initial heating.
-        while ((time.time() - start_t) < heat_time):
-            if ((time.time() - curr_t) >= data_log_freq):
-                t_center = thm.read(thm1)
-                t_edge = thm.read(thm2)
-
-                curr_t = time.time()
-                log.write_line_to_log(t_center, t_edge, pwm_center, pwm_edge, curr_t, start_t, cent_temps, edge_temps, times)
-
-        # Update PWM values to zero.
-        pwm_center = 0
-        pwm_edge = 0
-        heater.change_duty(pwm_center, pwm_edge, pwm_1, pwm_2)
-
-
-        print('Initial heating finished...')
-        log.write('LINE', round((time.time() - start_t), 2), 'Initial heating finished at: ')
-
-        pid_start_t = time.time()
-
-        # Wait for the temperature to settle.
-        while ((time.time() - pid_start_t) < wait_time):
-            if ((time.time() - curr_t) >= data_log_freq):
-                t_center = thm.read(thm1)
-                t_edge = thm.read(thm2)
-
-                curr_t = time.time()
-                log.write_line_to_log(t_center, t_edge, pwm_center, pwm_edge, curr_t, start_t, cent_temps, edge_temps, times)
-
-
-        print('PID controller started ...')
-        log.write('LINE', round((time.time() - start_t), 2), 'PID Controller started at: ')
-
-        working = True
-        limited = [[False, False], [False, False]]
-
-        while working:
-            t_center_last = t_center
-            t_edge_last = t_edge
-            t_center = thm.read(thm1)
-            t_edge = thm.read(thm2)
-
-            if ((time.time() - curr_t) >= data_log_freq):
-
-                curr_t = time.time()
-                log.write_line_to_log(t_center, t_edge, pwm_center, pwm_edge, curr_t, start_t, cent_temps, edge_temps, times)
-
-            t_center_avg =  (t_center + t_center_last) / 2.0
-            t_edge_avg =  (t_edge + t_edge_last) / 2.0
-
-            pid_center.update(t_center_avg)
-            pwm_center = pid_center.output
-            pwm_center = heater.clamp(pwm_center, 0, 100)
-
-            pid_edge.update(t_edge_avg)
-            pwm_edge = pid_edge.output
-            pwm_edge = heater.clamp(pwm_edge, 0, 100)
-
-            heater.change_duty(pwm_center, pwm_edge, pwm_1, pwm_2)
-
-            # Suppress Kp once the current temp nears the working temp.
-            if ((limited[0][0] == False) and ((work_temp - t_center_avg) < 15)):
-                print("Kp center suppressed ... ")
-                log.write('LINE', 0, 'Kp center suppressed')
-                pid_center.setKp(limited_kp)
-                limited[0][0] = True
-
-            if ((limited[1][0] == False) and ((work_temp - t_edge_avg) < 15)):
-                print("Kp edge suppressed ... ")
-                log.write('LINE', 0, 'Kp edge suppressed')
-                pid_edge.setKp(limited_kp)
-                limited[1][0] = True
-
-            if ((limited[0][1] == False) and ((work_temp - t_center_avg) < 1)):
-                print("Kd center suppressed ... ")
-                log.write('LINE', 0, 'Kd center suppressed')
-                pid_center.setKd(limited_kd)
-                limited[0][1] = True
-
-            if ((limited[1][1] == False) and ((work_temp - t_edge_avg) < 1)):
-                print("Kd edge suppressed ... ")
-                log.write('LINE', 0, 'Kd edge suppressed')
-                pid_edge.setKd(limited_kd)
-                limited[1][1] = True
+    P_edge = window.P_edge
+    I_edge = window.I_edge
+    D_edge = window.D_edge
 
 
 
-
-    except KeyboardInterrupt:
-        log.close()
-        thm.close(thm1)
-        thm.close(thm2)
-        heater.close(pwm_1)
-        heater.close(pwm_2)
-
-        coefficients_center = pid_center.getPID()
-        coefficients_edge = pid_edge.getPID()
-
-        log.createPlot(times, cent_temps, edge_temps, heat_time, coefficients_center, coefficients_edge, pid_center_val, pid_edge_val)
-
-        sys.exit()
-
-    except:
-        traceback.print_exc()
-        log.close()
-        thm.close(thm1)
-        thm.close(thm2)
-        heater.close(pwm_1)
-        heater.close(pwm_2)
-
-        sys.exit()
+ 
--- thmcouple.py
+++ thmcouple.py
@@ -1,63 +1,75 @@
 '''
 
     ************************************************************************
-    *   FILE NAME:      thmcouple.py
+    *   FILE NAME:      temp_rtd.py
     *   AUTHORS:         Dylan Vogel, Peter Feng
-    *   PURPOSE:        This file contains functions related to thermocouple
-    *                   measurement via the MAX31855.
+    *   PURPOSE:        This file contains functions related to RTD
+    *                   measurement via the MAX31865.
     *
     *   EXTERNAL REFERENCES:    spidev
     *
     *
-    *   NOTES:          The read function could be modified to read 32 bits instead
-    *                   of 16 and check what type of fault is encountered.
+    *   NOTES:         Should write in fault detection functions.
+    *                   DOESN'T HANDLE NEGATIVE VALUES YET
     *
     *   REVISION HISTORY:
     *
-    *                   2017-05-22: Wrote setup and read functions.
-    *                   2017-05-23: Added comments.
+    *                   2017-07-26: Copied file from thmcouple.py
+    *
 
 '''
 import spidev
+import RPi.GPIO as GPIO
+import time
+import math
 
-global READBYTE
-READBYTE = [0x00, 0x00]
+CONFIG_ADDR = 0x80
+CONFIG = 0b10100001  # write this before reading.
+RTD_MSB_ADDR  = 0x01
+RTD_A = 0.0039083
+RTD_B = -0.0000005775
 
 #Set up 1st thermocouple
 def setup1():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25, GPIO.IN)
    tc_1 = spidev.SpiDev()
+   tc_1.mode = 0b01
    tc_1.open(0,0)
    tc_1.max_speed_hz = 5000000
    return tc_1
 
 #Set up 2nd thermocouple
 def setup2():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25, GPIO.IN)
    tc_2 = spidev.SpiDev()
+   tc_2.mode = 0b01
    tc_2.open(0,1)
    tc_2.max_speed_hz = 5000000
    return tc_2
 
-def read(thmcouple):
-    # Assume the input is either 1 or 2, and default to 1 if another value is entered.
-    rec = thmcouple.xfer2(READBYTE)
 
-    # Check the fault bit of the returned message
-    if rec[1] & 0x01:
-        fault = 1
-        #print "A fault was encountered by the MAX31855"
-    else:
-        fault = 0
+def read(sensor):
+    sensor.xfer2([CONFIG_ADDR, CONFIG])
+    while not GPIO.input(25):
+        time.delayMicroseconds(1)
 
-    # Convert the 16-bit returned number to the 14-bit temp value.
-    data = (rec[0] << 6) | (rec[1] >> 2)
+    rec = sensor.xfer2([RTD_MSB_ADDR, 0x00, 0x00])
+    data = (rec[1] << 8 | rec[2] >> 1)
 
-    # Check for negatives
-    if data >> 13:
-        temp = float((int(data) / 4.0) - 2048)
-    else:
-        temp = float(data / 4.0)
+    Rt = (data * 430)
 
-    return temp
+    Z1 = -RTD_A
+    Z2 = RTD_A * RTD_A - (4 * RTD_B)
+    Z3 = (4 * RTD_B) / 100
+    Z4 = 2 * RTD_B
 
-def close(thmcouple):
-    thmcouple.close()
+    temp = Z2 + (Z3 * Rt)
+    temp = (math.sqrt(temp) + Z1) / Z4
+
+    if (temp >= 0):
+        return temp
+
+def close(sensor):
+    sensor.close()
